326459b52fff74e5702e3351ed9352399b4c326f
Lasse Lykke Jensen <lasselj@yandex.com>
2021-11-23 14:22:40 +0100

    Changes

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <img src="funny monkey.png" id="frontimg">
+    <img src="img/funny monkey.png" id="frontimg">
+      <div class="topnav">
+          <a class="active" href="#home">Home</a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+      </div>
     <h1>CafeE</h1>
   </body>
 </html>
