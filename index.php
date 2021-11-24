0de289607148e0c3687993d343a5db19280c33fb
Rasmus Frølund Larsen <rasmusflarsen@hotmail.com>
2021-11-24 10:19:33 +0100

    big-button

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
     <img src="img/funny monkey.png" id="frontimg">
     <img src="img/skate_monke.png" id="frontimg">
-   <a class="big-buttom" href='https://www.youtube.com/'> Bestil her </a>
+   <a class="big-button" href='https://www.youtube.com/'> Bestil her </a>
    
    <div id="footer">This is a footer this stays at the bottom of the page :D
     <h1>Kontakt i åbningstiden tlf:7629 2378 eller tlf:2492 8090
