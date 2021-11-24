e726206ad59e993299ee0e6a3eab3aa591422d06
Lasse Lykke Jensen <lasselj@yandex.com>
2021-11-24 11:05:47 +0100

    Image behind header

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="index.css">
-    </head>
-    <body>
+
+<head>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div id="top-image-header">
         <div id="header">
             <h1>Café E</h1>
             <div class="topnav">
                 <div class="buttons">
-                    <a href="#home">Bestil her</a>
-                    <a href="#news">Åbningstider</a>
-                    <a href="#contact">Om os</a>
-                    <a href="#about">Kontakt os</a>
+                    <a href="#menu">Menu</a>
+                    <a href="#bestil">Bestil her</a>
+                    <a href="#aabningstider">Åbningstider</a>
+                    <a href="#om">Om os</a>
+                    <a href="#kontakt">Kontakt os</a>
                 </div>
             </div>
         </div>
         <img src="img/funny monkey.png" id="frontimg">
-        <img src="img/skate_monke.png" id="frontimg">
+    </div>
 
-        <div class="bestil-her-div"> 
-            <a class="big-button" href='https://www.youtube.com/'> Bestil her </a>
-        </div>
-            <div id="footer">This is a footer this stays at the bottom of the page :D
-            <h1 id="footer-tlf">Kontakt i åbningstiden tlf:7629 2378 eller tlf:2492 8090</h1>
-            <a href="http://www.findsmiley.dk/1112436">fødevarerstyrelsen<img src="img/1Smiley.png"></a>
-            <a href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
-            </div>
-    </body>
+    <div class="bestil-her-div">
+        <a class="big-button" href='https://www.youtube.com/'> Bestil her </a>
+    </div>
+    <div id="footer">This is a footer this stays at the bottom of the page :D
+        <h1 id="footer-tlf">Kontakt i åbningstiden tlf:7629 2378 eller tlf:2492 8090</h1>
+        <a href="http://www.findsmiley.dk/1112436">fødevarerstyrelsen<img src="img/1Smiley.png"></a>
+        <a href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
+    </div>
+</body>
+
 </html>
