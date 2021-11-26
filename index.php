af4b7be84398ea6eaa90092519052270913aa3a1
Lasse Lykke Jensen <lasselj@yandex.com>
2021-11-26 12:23:03 +0100

    fix footer for the LAST TIME!

--- index.php
+++ index.php
@@ -6,28 +6,30 @@
 </head>
 
 <body>
-    <div id="top-image-header">
-        <div id="header">
-            <h1>Café E</h1>
-            <div class="topnav">
-                <div class="buttons">
-                    <a class="right-line" href="#menu">Menu</a>
-                    <a class="right-line" href="#bestil">Bestil her</a>
-                    <a class="right-line" href="#aabningstider">Åbningstider</a>
-                    <a class="right-line" href="#om">Om os</a>
-                    <a href="#kontakt">Kontakt os</a>
+    <div id="page-container">
+        <div id="top-image-header">
+            <div id="header">
+                <h1>Café E</h1>
+                <div class="topnav">
+                    <div class="buttons">
+                        <a class="right-line" href="#menu">Menu</a>
+                        <a class="right-line" href="#bestil">Bestil her</a>
+                        <a class="right-line" href="#aabningstider">Åbningstider</a>
+                        <a class="right-line" href="#om">Om os</a>
+                        <a href="#kontakt">Kontakt os</a>
+                    </div>
                 </div>
             </div>
+            <img src="img/cafe-bg.png" id="frontimg">
         </div>
-        <img src="img/cafe-bg.png" id="frontimg">
-    </div>
-    <div class="bestil-her-div">
-        <a class="big-button" href='https://www.youtube.com/'> Bestil her </a>
-    </div>
-    <div id="footer">This is a footer this stays at the bottom of the page :D
-        <h1 id="footer-tlf">Kontakt i åbningstiden tlf:7629 2378 eller tlf:2492 8090</h1>
-        <a id="footersmiley" href="http://www.findsmiley.dk/1112436">fødevarerstyrelsen<img src="img/Smiley.png"></a>
-        <a id="footertlf" href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
+        <div class="bestil-her-div">
+            <a class="big-button" href='https://www.youtube.com/'> Bestil her </a>
+        </div>
+        <div id="footer">This is a footer this stays at the bottom of the page :D
+            <h1 id="footer-tlf">Kontakt i åbningstiden tlf:7629 2378 eller tlf:2492 8090</h1>
+            <a id="footersmiley" href="http://www.findsmiley.dk/1112436">fødevarerstyrelsen<img src="img/Smiley.png"></a>
+            <a id="footertlf" href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
+        </div>
     </div>
 </body>
 
