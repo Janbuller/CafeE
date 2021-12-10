ac5bb382a75a33c35ad83791c91e169edc9a55dd
Lasse Lykke Jensen <lasselj@yandex.com>
2021-12-10 11:46:16 +0100

    Fully fix footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer id="footer">
     <p id="footer-tlf">Telefon:7629 2378 eller 2492 8090</p>
-    <a id="footertlf" href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
+    <p id="footer-addr">Carolinelundsvej 2, 8700 Horsens</p>
+    <a id="footer-mail" href="mailto:info@kokkenudafhuset.dk">Mail: info@kokkenudafhuset.dk </a>
     <a id="footersmiley" href="http://www.findsmiley.dk/1112436">
         <img src="img/smiley.webp">
     </a>
