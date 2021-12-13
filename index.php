05b47e4d326c96136639e259917191380a748baa
Lasse Lykke Jensen <lasselj@yandex.com>
2021-12-13 10:27:30 +0100

    fix lorem ipsum

--- index.php
+++ index.php
@@ -25,11 +25,41 @@ $img_name = "img/cafe-bg.webp";
             <div class="ideen-bag-text-div">
              <h2> information om kafeén </h2>
              <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate efficitur sagittis. Proin felis tellus, congue ac porttitor vel, iaculis ac nunc. Praesent aliquet eros at diam sollicitudin, quis aliquam erat vehicula. In nisl lorem, congue sit amet enim a, pretium euismod sem. Sed leo ligula, ultricies eu magna vel, tristique bibendum est. Sed cursus ornare imperdiet. Morbi urna ipsum, porta sit amet mattis vitae, euismod a felis. Mauris sit amet diam in ipsum commodo accumsan nec non urna.</p>
-
-<p>In ultrices massa in tincidunt pretium. Ut sit amet arcu eu tortor lobortis hendrerit. Maecenas quis quam enim. Nunc at augue scelerisque, lobortis lectus vitae, pharetra lacus. Nunc accumsan magna vitae sem volutpat, vel sagittis lorem finibus. Sed egestas imperdiet dolor non tincidunt. Donec volutpat sem in libero lacinia mollis. Sed imperdiet euismod porttitor. Vestibulum condimentum purus eget turpis iaculis, et volutpat neque scelerisque. Nulla vitae malesuada felis. Quisque ultricies in enim eget aliquet. Pellentesque ut fringilla nunc, quis pellentesque nisl. Mauris eleifend luctus ante sit amet consequat. Mauris quis sem sit amet orci sagittis egestas.</p>
-
-<p>Proin a maximus ex. Nam laoreet iaculis quam. Maecenas euismod massa vitae ante finibus, in ullamcorper eros tristique. Nunc fringilla faucibus elit vitae finibus. Proin elementum diam sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut eu diam tempus, malesuada arcu nec, maximus massa. Nulla sed convallis nisl, ac tristique tortor. Donec quis porta justo. Donec ut ornare ligula. Vestibulum at egestas lacus. Ut at ante sit amet orci condimentum tristique eu vitae mi. Sed commodo velit bibendum nisl egestas malesuada. Duis interdum bibendum convallis.</p>
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                 vulputate efficitur sagittis. Proin felis tellus, congue ac
+                 porttitor vel, iaculis ac nunc. Praesent aliquet eros at diam
+                 sollicitudin, quis aliquam erat vehicula. In nisl lorem, congue
+                 sit amet enim a, pretium euismod sem. Sed leo ligula, ultricies
+                 eu magna vel, tristique bibendum est. Sed cursus ornare
+                 imperdiet. Morbi urna ipsum, porta sit amet mattis vitae,
+                 euismod a felis. Mauris sit amet diam in ipsum commodo accumsan
+                 nec non urna.
+             </p>
+             <p>
+                 In ultrices massa in tincidunt pretium. Ut sit amet arcu eu
+                 tortor lobortis hendrerit. Maecenas quis quam enim. Nunc at
+                 augue scelerisque, lobortis lectus vitae, pharetra lacus. Nunc
+                 accumsan magna vitae sem volutpat, vel sagittis lorem finibus.
+                 Sed egestas imperdiet dolor non tincidunt. Donec volutpat sem
+                 in libero lacinia mollis. Sed imperdiet euismod porttitor.
+                 Vestibulum condimentum purus eget turpis iaculis, et volutpat
+                 neque scelerisque. Nulla vitae malesuada felis. Quisque
+                 ultricies in enim eget aliquet. Pellentesque ut fringilla nunc,
+                 quis pellentesque nisl. Mauris eleifend luctus ante sit amet
+                 consequat. Mauris quis sem sit amet orci sagittis egestas.
+             </p>
+             <p>
+                 Proin a maximus ex. Nam laoreet iaculis quam. Maecenas euismod
+                 massa vitae ante finibus, in ullamcorper eros tristique. Nunc
+                 fringilla faucibus elit vitae finibus. Proin elementum diam
+                 sapien. Class aptent taciti sociosqu ad litora torquent per
+                 conubia nostra, per inceptos himenaeos. Ut eu diam tempus,
+                 malesuada arcu nec, maximus massa. Nulla sed convallis nisl, ac
+                 tristique tortor. Donec quis porta justo. Donec ut ornare
+                 ligula. Vestibulum at egestas lacus. Ut at ante sit amet orci
+                 condimentum tristique eu vitae mi. Sed commodo velit bibendum
+                 nisl egestas malesuada. Duis interdum bibendum convallis.
+             </p>
              </p>
             </div>
         </div>
